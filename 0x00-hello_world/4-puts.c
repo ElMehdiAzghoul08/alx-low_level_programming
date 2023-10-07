f906d38d5ca30f1d07cc7f11cd8f32fd9ec4a32b
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-07 11:45:13 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main -Short description, single line
+ *
+ * Desctiption: Longer description of the function)?
+ *
+ * Return: Description of the returned value
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
