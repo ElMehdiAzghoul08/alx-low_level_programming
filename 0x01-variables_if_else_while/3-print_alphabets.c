d5c9373a0abcaa514e602660efee8557bfdf792b
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-09 19:04:16 -0700

    letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description : alphabet letters
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 97;
+
+	while (ch <= 122)
+
+	char ch = 65;
+
+	while (ch < = 90)
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
