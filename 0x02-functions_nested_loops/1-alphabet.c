0eaaa068499102db950f6c917287ca6152a9abf7
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-11 15:03:41 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int pt;
+
+	for (pt = 97 ; pt <= 122 ; pt++)
+	{
+		putchar(pt);
+		putchar('\n');
+			return (0);
+	}
+}
