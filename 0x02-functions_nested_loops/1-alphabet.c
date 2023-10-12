3d0db368a6c0d2652455dcecd204d37d38e0b0e2
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-11 18:57:48 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet - prints all alphabet in lowercase from a -z
- *
+ * lowecase letters are printed by the use of the function _putchar
  */
 
 void print_alphabet(void)
@@ -10,6 +10,6 @@ void print_alphabet(void)
 	int pr;
 
 	for (pr = 'a' ; pr <= 'z'; pr++)
-		putchar(pr);
-			putchar('n');
+		_putchar(pr);
+			_putchar('\n');
 }
