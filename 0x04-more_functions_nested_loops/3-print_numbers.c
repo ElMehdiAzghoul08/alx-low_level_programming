8e1edbdc0065b5729655e677e357b59fbe4355f4
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-12 16:31:59 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - is a function that prints numbers from 0 - 9
+ *
+*/
+void print_numbers(void)
+{
+	char (pt = '0');
+
+	while
+		(pt = '9');
+
+	{
+		_putchar(pt)
+			pt++;
+	}
+	_putchar('\n');
+}
