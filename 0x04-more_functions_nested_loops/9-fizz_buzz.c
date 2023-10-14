d651b5ee18a0f3e8b3e782f3d1ec50235120ca3c
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-13 19:42:11 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - a function that print numbers from 1-100
+ *
+ * Return: 0
+ *
+*/
+
+int main(void)
+{
+	int r;
+
+	for (r = 1; r <= 100; r++)
+	{
+	if (r % 3 == 0)
+	printf("Fizz");
+	else if (r % 5 == 0)
+	printf("Buzz");
+	else if ((r % 3 == 0) && (r % 5 == 0))
+	printf("FizzBuzz");
+	else
+		printf("%d", r);
+	if (r != 100)
+		printf(32);
+	else
+		printf('\n');
+
+	}
+
+	return (0);
+}
