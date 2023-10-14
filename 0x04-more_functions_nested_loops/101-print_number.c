0a3bad0ddd913d1b9e3d5f416617faa2500965e1
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-13 21:32:20 -0700

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,11 +12,10 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar(45);
-
 		r = -r;
 	}
 	if ((r / 10) > 0)
-		printf_number(r / 10);
+		print_number(r / 10);
 
 	_putchar((r % 10) + '0');
 }
