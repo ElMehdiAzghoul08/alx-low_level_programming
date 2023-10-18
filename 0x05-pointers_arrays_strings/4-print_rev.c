54daead339b96ca5ab0f9f01e42c1c1cd1f93be1
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-18 13:32:30 -0700

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,11 @@
 
 void print_rev(char *s)
 {
-	int r, v, e;
-
-	v = strlen(s);
-	for (r = 0; r < v / 2; r++)
+	if (*s)
 	{
-		e = s[r];
-		s[r] = s[v - r - 1];
-		s[v - r - 1] = e;
+	_putchar(s + 1);
+	_putchar("%c", *s);
+
 	}
 
 }
