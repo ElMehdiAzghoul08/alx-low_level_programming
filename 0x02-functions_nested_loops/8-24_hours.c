731c50468498bfffec4228000ac175068c9b7f79
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-20 10:27:33 -0700

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,11 +4,13 @@
  * jack_bauer - a function that prints every minutes of the day of Jack Bauer
  */
 void jack_bauer(void)
-{
-	int o = 0;
-	int l = 0;
 
-	while (l >= 23)
+{
+	int o, l;
+
+	for (o = 0; o <= 23; o++)
+	{
+	for (l = 0; l <= 59; l++)
 	{
 		char r = (o / 10) + 48;
 		char v = (o % 10) + 48;
@@ -21,12 +23,6 @@ void jack_bauer(void)
 		_putchar(mr);
 		_putchar(mv);
 		_putchar('\n');
-	l++;
-
-	if (l == 59)
-	{
-	l = 0;
-	o++;
 	}
 }
 }
