b884b983b5d9dc6c6740381546ef2062b1edaa04
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-21 13:10:08 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,36 +8,33 @@ void times_table(void)
 	int r, v, e;
 
 	r = 0;
-	do
-
-	{
-	v = 0;
-	do
-
-	{
-	e = r * v;
-
-	if
-	(v > 0)
-
-	{
-			_putchar(44);
-			_putchar(32);
+	do {
+		v = 0;
+		do {
+			e = r * v;
+			if (v > 0)
+			{
+				_putchar(44);
+				_putchar(32);
 
 			if (e < 10)
 			{
-			_putchar(32);
+				_putchar(32);
+				_putchar(e % 10 + 48);
 			}
-		}
-			_putchar(e / 10 + 48);
-			_putchar(e % 10 + 48);
-
+			else
+			{
+				_putchar(e / 10 + 48);
+				_putchar(e % 10 + 48);
+			}
+			}
+			else
+			{
+			_putchar(48);
+			}
 			v++;
-	}
-		while (v <= 9);
-
-			_putchar('\n');
-			r++;
-}
-	while (r <= 9);
+		} while (v <= 9);
+		_putchar('\n');
+		r++;
+		} while (r <= 9);
 }
