6fefbab59ab5e75052100b72468efee6c3a41a6b
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-24 18:36:43 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concenate two strings.
+ * @dest: input
+ * @src: input
+ *
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int r = 0;
+	int v = 0;
+
+while (dest[r] != '\0')
+{
+	r++;
+}
+	do {
+
+	dest[r] = str[v];
+	r++;
+	v++;
+
+	} while (src[v] != '\0');
+
+	dest[r] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *scr, int n);
+char *_strncpy(char *dest, char *scr, int n);
+int _strcmp(char *s1, char *s2);
