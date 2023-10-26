b70005b29725399a34d4bd14d5ecf470c99325ae
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-26 10:49:23 -0700

    commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - is a function that prints a string
+ *
+ * @s: input
+ *
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	while (*s != '\0')
+	{
+	_putchar(*s);
+
+	s++
+	}
+	_putchar('\n');
+}
