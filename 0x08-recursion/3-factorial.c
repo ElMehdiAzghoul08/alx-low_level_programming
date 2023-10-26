b5f7d2d5cd1b66ecab49bb22cc3f580abc8a57a1
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-26 13:16:56 -0700

    commit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,14 +11,10 @@
 int factorial(int n)
 {
 	if (n == 0 || n == 1)
+
 	{
 		return (1);
 	}
-	else if (n > 1)
-	{
-		return (n * factorial(n - 1));
-	}
-	return (n * factorial(n - 1));
 
 	else if (n > 1)
 	{
