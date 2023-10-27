0b15e05fd6d211b024af4aa53962531d1c83a1f5
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-26 21:38:02 -0700

    commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,29 @@
 #include <string.h>
 
 /**
- * _sqrt_recursion -is a function that returns square root of a number
+ * sqrt__recursion - function that returns the natural square root of a number
+ *
+ * @n: input
+ * @r: input
+ *
+ * Return: the natural root of a number otherwise -1
+ */
+
+int sqrt__recursion(int n, int r)
+{
+	if (r * r == n)
+	{
+		return (r);
+	}
+	if (r * r > n)
+	{
+		return (-1);
+	}
+	return (sqrt__recursion(n, r + 1));
+}
+
+/**
+ * _sqrt_recursion -  function that returns the natural square root of a number
  *
  * @n: input
  *
@@ -13,44 +35,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
-	return (-1);
-	}
-
-	if (n == 0 || n == 1)
-	{
-	return (n);
-	}
-
-	return (_sqrt(n, n / 2));
-}
-
-/**
- * _sqrt - a function that returns the natural square root of a number
- *
- * @n: input
- * @r: input
- *
- * Return: the natural root of a number otherwise -1
- */
-
-int _sqrt(int n, int r)
-{
-	if (r * r == n)
-
-	{
-		return (r);
-		}
-		if (r <= 0)
-
-		{
 		return (-1);
-		}
-		int theone = (r + n / r) / 2;
-
-		if (theone >= r)
-
-		{
-		return (-1);
-		}
-	return (_sqrt(n, theone));
+	}
+	return (sqrt_recursion(n, 0));
 }
