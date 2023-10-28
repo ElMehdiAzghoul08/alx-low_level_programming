6e02d1e8547e1d36f852e1e78ed1bf57dcfa0ee5
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-28 10:14:39 -0700

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - is a function that print its name
+ *
+ * @argc: input
+ * @argv: input
+ *
+ * Return: the function name
+ */
+
+int main(int __attribute__((unused))argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
