372ca3e906cbbf60a185b78d25c2eb9783dd585e
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-10-28 10:35:21 -0700

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print the number of arguments passed into it
+ *
+ * @argc: input
+ * @argv: input
+ *
+ * Return: number of arguments
+ */
+
+int main(int argc,char __attribute__((unused))*argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
