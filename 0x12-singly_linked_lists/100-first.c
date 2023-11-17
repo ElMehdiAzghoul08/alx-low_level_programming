ba2cfb8130c834e32e0bb768db34c9d6925e28d0
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-11-16 19:13:57 -0800

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * jst__print - a function
+ * Return: void
+ */
+
+void jst__print(void)
+{
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
+}
