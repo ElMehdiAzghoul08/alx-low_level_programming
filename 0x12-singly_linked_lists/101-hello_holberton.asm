206a4676fd71b4ba028424b28fa7cc987a6d7c5d
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-11-16 20:53:33 -0800

    assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,6 @@
 section .data
     hello db 'Hello, Holberton', 0
+    newline db 10
 
 section .text
     global main
@@ -11,6 +12,10 @@ main:
     sub rsp, 8
     mov rdi, hello
     call printf
+
+    mov rdi, newline
+    call printf
+
     add rsp, 8
 
     mov rdi, 0
