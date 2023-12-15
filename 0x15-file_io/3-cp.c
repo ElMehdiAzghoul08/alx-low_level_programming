e91a6cc587cd324878c36b4493a6ac223b45d063
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-12-15 09:27:13 -0800

    fix last attempt

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -38,37 +38,27 @@ int main(int argc, char *argv[])
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		justexit_therror(98, "", argv[1]);
-	}
-	file_to_ = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	} file_to_ = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to_ == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		justexit_therror(99, "", argv[2]);
-	}
-	do
-	{
+	} do {
 		rb = read(file_from_, b, _SIZE_b);
 		if (rb == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			justexit_therror(98, "", argv[1]);
-		}
-		wb = write(file_to_, b, rb);
+		} wb = write(file_to_, b, rb);
 		if (wb == -1 || wb != rb)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			justexit_therror(99, "", argv[2]);
 		}
 	} while (rb == _SIZE_b);
-	if (close(file_from_) == -1)
+	if (close(file_from_) == -1 || close(file_to_) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from_);
+		dprintf(STDERR_FILENO, "Error: Can't close file descriptors\n");
 		justexit_therror(100, "", "");
-	}
-	if (close(file_to_) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to_);
-		justexit_therror(100, "", "");
-	}
-	return 0;
+	} return 0;
 }
