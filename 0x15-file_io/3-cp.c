1c1ba0e59e1b98853acae64c33a4b0536e55c50e
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-12-15 09:00:09 -0800

    file io fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,21 +4,18 @@
 #include <stdlib.h>
 
 #define _SIZE_b 1024
-
 /**
- * exit_error - a function
+ * jstexit_therror - a function
  * @_the_program: input
  * @the_content_: input
  * @name_of_file: input
- * Return: void
  */
-
-void exit_error(int _the_program, const char *the_content_, const char *name_of_file)
+void justexit_therror(int _the_program, const char *the_content_,
+		const char *name_of_file)
 {
 	dprintf(STDERR_FILENO, the_content_, name_of_file);
 	exit(_the_program);
 }
-
 /**
  * main - a function
  * @argc: input
@@ -33,44 +30,45 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		exit_error(97, "Usage: %s file_from_ file_to\n");
+		dprintf(STDERR_FILENO, "Usage: %s file_from_ file_to\n", argv[0]);
+		exit(97);
 	}
-
 	file_from_ = open(argv[1], O_RDONLY);
 	if (file_from_ == -1)
-		exit_error(98, "Error: Can't read from file %s\n", argv[1]);
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		justexit_therror(98, "", argv[1]);
+	}
 	file_to_ = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to_ == -1)
 	{
-		close(file_from_);
-		exit_error(99, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		justexit_therror(99, "", argv[2]);
 	}
-
-	do {
+	do
+	{
 		rb = read(file_from_, b, _SIZE_b);
 		if (rb == -1)
 		{
-			close(file_from_);
-			close(file_to_);
-			exit_error(98, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			justexit_therror(98, "", argv[1]);
 		}
 		wb = write(file_to_, b, rb);
 		if (wb == -1 || wb != rb)
 		{
-			close(file_from_);
-			close(file_to_);
-			exit_error(99, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			justexit_therror(99, "", argv[2]);
 		}
 	} while (rb == _SIZE_b);
-
 	if (close(file_from_) == -1)
-
-		exit_error(100, "Error: Can't close fd %d\n", file_from_);
-
-		if (close(file_to_) == -1)
-		exit_error(100, "Error: Can't close fd %d\n", file_to_);
-
-
-	return (0);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from_);
+		justexit_therror(100, "", "");
+	}
+	if (close(file_to_) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to_);
+		justexit_therror(100, "", "");
+	}
+	return 0;
 }
