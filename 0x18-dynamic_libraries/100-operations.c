33a551b8d12aab3b14e5a23602e852c99da70c00
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-01-15 18:54:57 -0800

    lets call c fixed

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,41 +1,22 @@
-#include <stdlib.h>
-/**
- * add - a function
- * @a: input
- * @b: input
- * Return: result
- */
 int add(int a, int b)
 {
 	return (a + b);
 }
 
-/**
- * sub - a function
- * @a: input
- * @b: input
- * Return: result
- */
 int sub(int a, int b)
 {
 	return (a - b);
 }
-/**
- * mod - a function
- * @a: input
- * @b: input
- * Return: result
- */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
 int mod(int a, int b)
 {
 	return (a % b);
 }
-/**
- * mul - a function
- * @a: input
- * @b: input
- * Return: result
- */
+
 int mul(int a, int b)
 {
 	return (a * b);
