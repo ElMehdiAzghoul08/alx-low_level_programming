f4ffbad53356105c6f397d147bb6b073cce35ea0
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-04-06 04:58:52 -0700

    update + betty fix

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,6 +9,7 @@
  * Return: an index.
  *
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size) {
-  return (hash_djb2(key) % size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+return (hash_djb2(key) % size);
 }
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,23 +6,27 @@
  * @ht: is the hash table.
  * Return: void.
  */
-void hash_table_delete(hash_table_t *ht) {
-  hash_table_t *head = ht;
-  hash_node_t *node, *tmp;
-  unsigned long int i;
+void hash_table_delete(hash_table_t *ht)
+{
+hash_table_t *head = ht;
+hash_node_t *node, *tmp;
+unsigned long int i;
 
-  for (i = 0; i < ht->size; i++) {
-    if (ht->array[i] != NULL) {
-      node = ht->array[i];
-      while (node != NULL) {
-        tmp = node->next;
-        free(node->key);
-        free(node->value);
-        free(node);
-        node = tmp;
-      }
-    }
-  }
-  free(head->array);
-  free(head);
+for (i = 0; i < ht->size; i++)
+{
+if (ht->array[i] != NULL)
+{
+node = ht->array[i];
+while (node != NULL)
+{
+tmp = node->next;
+free(node->key);
+free(node->value);
+free(node);
+node = tmp;
+}
+}
+}
+free(head->array);
+free(head);
 }
