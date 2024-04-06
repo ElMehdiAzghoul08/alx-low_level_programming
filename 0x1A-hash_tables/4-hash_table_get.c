4030f2eb44ef1f2a205fbdabe834e501fff58158
El Mehdi <azghoulmehdi899@gmail.com>
2024-04-06 11:28:27 +0200

    file update + fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -2,29 +2,27 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - Retrieve the value associated with
- *                  a key in a hash table.
- * @ht: A pointer to the hash table.
- * @key: The key to get the value of.
+ * hash_table_get - retrieves a value associated with a key.
  *
- * Return: If the key cannot be matched - NULL.
- *         Otherwise - the value associated with key in ht.
+ * @ht: hash table to unlock.
+ * @key: the key.
+ *
+ * Return: the value or NULL.
  */
-char *hash_table_get(const hash_table_t *ht, const char *key)
-{
-	hash_node_t *node;
-	unsigned long int index;
+char *hash_table_get(const hash_table_t *ht, const char *key) {
+  hash_node_t *node;
+  unsigned long int index;
 
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (NULL);
+  if (ht == NULL || key == NULL || *key == '\0')
+    return (NULL);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	if (index >= ht->size)
-		return (NULL);
+  index = key_index((const unsigned char *)key, ht->size);
+  if (index >= ht->size)
+    return (NULL);
 
-	node = ht->array[index];
-	while (node && strcmp(node->key, key) != 0)
-		node = node->next;
+  node = ht->array[index];
+  while (node && strcmp(node->key, key) != 0)
+    node = node->next;
 
-	return ((node == NULL) ? NULL : node->value);
+  return ((node == NULL) ? NULL : node->value);
 }
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -3,29 +3,26 @@
 
 /**
  * hash_table_delete - Deletes a hash table.
- * @ht: A pointer to a hash table.
+ * @ht: is the hash table.
+ * Return: void.
  */
-void hash_table_delete(hash_table_t *ht)
-{
-	hash_table_t *head = ht;
-	hash_node_t *node, *tmp;
-	unsigned long int i;
+void hash_table_delete(hash_table_t *ht) {
+  hash_table_t *head = ht;
+  hash_node_t *node, *tmp;
+  unsigned long int i;
 
-	for (i = 0; i < ht->size; i++)
-	{
-		if (ht->array[i] != NULL)
-		{
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				tmp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = tmp;
-			}
-		}
-	}
-	free(head->array);
-	free(head);
+  for (i = 0; i < ht->size; i++) {
+    if (ht->array[i] != NULL) {
+      node = ht->array[i];
+      while (node != NULL) {
+        tmp = node->next;
+        free(node->key);
+        free(node->value);
+        free(node);
+        node = tmp;
+      }
+    }
+  }
+  free(head->array);
+  free(head);
 }
