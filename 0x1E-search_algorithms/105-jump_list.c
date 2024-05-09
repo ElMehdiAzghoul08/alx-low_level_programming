049904b03f2ab764720b8479f7e602b526cf8bc3
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-05-08 22:09:04 -0700

    betty fix2

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -11,34 +11,34 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-    size_t mv = sqrt(size), past_ = 0;
-    listint_t *prst = list;
-    size_t a;
+size_t mv = sqrt(size), past_ = 0;
+listint_t *prst = list;
+size_t a;
 
-    if (list == NULL)
-        return NULL;
+if (list == NULL)
+return (NULL);
 
-    while (prst && prst->n < value)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", prst->index, prst->n);
-        past_ = prst->index;
-        for (a = 0; prst->next && a < mv; a++)
-            prst = prst->next;
-    }
+while (prst && prst->n < value)
+{
+printf("Value checked at index [%lu] = [%d]\n", prst->index, prst->n);
+past_ = prst->index;
+for (a = 0; prst->next && a < mv; a++)
+prst = prst->next;
+}
 
-    printf("Value found between indexes [%lu] and [%lu]\n", past_, prst->index);
-    printf("Value checked at index [%lu] = [%d]\n", past_, ((listint_t *)list)->n);
+printf("Value found between indexes [%lu] and [%lu]\n", past_, prst->index);
+printf("Value checked at index [%lu] = [%d]\n", past_, ((listint_t *)list)->n);
 
-    while (list && list->index < past_)
-        list = list->next;
+while (list && list->index < past_)
+list = list->next;
 
-    while (list && list->index <= prst->index)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
-        if (list->n == value)
-            return list;
-        list = list->next;
-    }
+while (list && list->index <= prst->index)
+{
+printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+if (list->n == value)
+return (list);
+list = list->next;
+}
 
-    return NULL;
+return (NULL);
 }
